dd98b397fb2f623763988a763ea215426a09bf0d
DavidRuff06 <dpruff06@gmail.com>
2023-09-07 11:09:39 -0500

    Respawn Ball On Wall touch

--- Assets/onClick.cs
+++ Assets/onClick.cs
@@ -1,18 +1,28 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.OpenXR.Input;
+using UnityEngine.XR.Interaction.Toolkit.Inputs.Interactions;
+using static UnityEditorInternal.ReorderableList;
 
-public class onClick : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+public class onClick : MonoBehaviour { 
+
+    InputActionReference pressedX = null ;
+
+    private void Awake()
     {
-        
+        pressedX.action.started += Toggle;
     }
+
+
 
     // Update is called once per frame
-    void Update()
+    private void Toggle(InputAction.CallbackContext context)
     {
-        
+        bool isActive = gameObject.activeSelf;
+        gameObject.SetActive(isActive);
     }
 }
+
--- Assets/respawnBall.cs
+++ Assets/respawnBall.cs
@@ -7,11 +7,24 @@ public class respawnBall : MonoBehaviour
 
     [SerializeField] Transform ball;
     [SerializeField] Transform respawnPoint;
+    [SerializeField] Rigidbody rb;
     // Start is called before the first frame update
 
-    private void OnTriggerEnter(Collider other)
+    private void OnCollisionEnter(Collision other)
     {
-        ball.transform.position = respawnPoint.transform.position;
+        respawnPlayer();
+        
 
     }
+
+
+
+    public void respawnPlayer()
+    {
+
+        rb.velocity = Vector3.zero;
+        ball.position = respawnPoint.position;
+        Physics.SyncTransforms();
+    }
+
 }
