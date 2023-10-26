cdd46aa27359c9eb91cd4de8e8e50ff9637bccb2
DavidRuff06 <dpruff06@gmail.com>
2023-10-26 11:01:39 -0500

    Fixed (I'm guessing) physics problem

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -7,7 +7,7 @@ using TMPro;
 
 
 public class Score : MonoBehaviour
-{
+    {
     [ShowInInspector]
     [ReadOnly]
     public List<TMP_Text> FirstThrowTexts = new List<TMP_Text>();
@@ -18,25 +18,26 @@ public class Score : MonoBehaviour
     public List<TMP_Text> SecondThrowTexts = new List<TMP_Text>();
     [ShowInInspector]
     public GameObject SecondThrowParent;
-    [ShowInInspector][ReadOnly]
+    [ShowInInspector]
+    [ReadOnly]
     public List<TMP_Text> FinalTexts = new List<TMP_Text>();
     [ShowInInspector]
     public GameObject FinalTextParent;
 
     // Start is called before the first frame update
     void Start()
-    {
+        {
         for (int i = 0; i < FirstThrowParent.transform.childCount; i++)
             {
             FirstThrowTexts.Add(FirstThrowParent.transform.GetChild(i).GetComponent<TMP_Text>());
             SecondThrowTexts.Add(SecondThrowParent.transform.GetChild(i).GetComponent<TMP_Text>());
             FinalTexts.Add(FinalTextParent.transform.GetChild(i).GetComponent<TMP_Text>());
             }
-    }
+        }
 
     // Update is called once per frame
     void Update()
-    {
-        
+        {
+
+        }
     }
-}
